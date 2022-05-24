e8eddfec545a7f82f08b74e00f37a6eb65bad490
Dylan Dsouza <dylan@Dylans-MacBook-Air.local>
2022-05-24 11:10:13 +0530

    small change

--- resources/views/speech_page.blade.php
+++ resources/views/speech_page.blade.php
@@ -11,7 +11,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>Speech Analysis</title>
+      <title>Recommend Songs</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
