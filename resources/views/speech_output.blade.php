ae4b63713dac18f9cfb81f62cbe3bb6eab08ec95
Dylan Dsouza <dylan@Dylans-MacBook-Air.local>
2022-05-24 20:12:42 +0530

    small change

--- resources/views/speech_output.blade.php
+++ resources/views/speech_output.blade.php
@@ -291,6 +291,6 @@
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
-      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
    </body>
 </html>
--- resources/views/speech_page.blade.php
+++ resources/views/speech_page.blade.php
@@ -312,7 +312,7 @@
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
-      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
       <script>
          function startDictation() {
            if (window.hasOwnProperty('webkitSpeechRecognition')) {
